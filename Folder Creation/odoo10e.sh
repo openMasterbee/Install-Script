d4883516bacb17727dac7c6ba5357acb65ed78ee
Joe Huang <joe@masterbee.co>
2016-11-20 21:17:37 -0500

    add chmod

--- Folder Creation/odoo10e.sh
+++ Folder Creation/odoo10e.sh
@@ -20,9 +20,9 @@ echo "Crerated /opt/data/odoo10e/config"
 mkdir /opt/data/odoo10e/auto-backups
 echo "Crerated /opt/data/odoo10e/auto-backups"
 
-chown -R 777 /opt/data/odoo10e
-echo "Crerated /opt/data/odoo10e/auto-backups"
-
+chmod -R 777 /opt/data/odoo10e
+echo "Made odoo10e Folder 777"
+echo ""
 
 
 echo "Now begain creating folder structure for let's encrypt"
@@ -36,6 +36,9 @@ echo "Crerated /opt/data/letsencrypt/verify"
 mkdir /opt/data/letsencrypt/certs
 echo "Crerated /opt/data/letsencrypt/certs"
 
+chmod -R 777 /opt/data/letsencrypt
+echo "Made letsencrypt Folder 777"
+echo ""
 
 echo "Now begain creating folder structure for postgres"
 
@@ -45,6 +48,9 @@ echo "Crerated /opt/data/postgres"
 mkdir /opt/data/postgres/database
 echo "Crerated /opt/data/postgres/database"
 
+chmod -R 777 /opt/data/postgres
+echo "Made postgres Folder 777"
+echo ""
 
 
 echo "Now begain creating folder structure for redis"
@@ -54,3 +60,7 @@ echo "Crerated /opt/data/redis"
 
 mkdir /opt/data/redis/storage
 echo "Crerated /opt/data/redis/storage"
+
+chmod -R 777 /opt/data/redis
+echo "Made redis Folder 777"
+echo ""
