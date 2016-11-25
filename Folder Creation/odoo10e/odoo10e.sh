aa4a801fc834ccfd306900401d65c0a06a732bda
Joe Huang <joe@masterbee.co>
2016-11-25 16:36:29 -0500

    add image pull sh as curl call

--- Folder Creation/odoo10e/odoo10e.sh
+++ Folder Creation/odoo10e/odoo10e.sh
@@ -91,22 +91,4 @@ echo ""
 echo "----------------------------------------------------------------"
 echo ""
 
-
-image_pull () {
-docker pull postgres:9.3
-docker pull redis
-docker pull nginx:alpine
-docker pull tozny/rancher-lets-encrypt
-docker pull razeen/odoo10e:v1.1
-}
-
-read -r -p "Do you wish to pull down all images? [y/N]" response
-case $response in
-    [yY][eE][sS]|[yY])
-      image_pull
-      echo "Image Pull Secussful."
-        ;;
-    *)
-      echo "Image pulling skiped. Process finish."
-        ;;
-esac
+curl -sSL https... | bash
