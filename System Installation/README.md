60357b3f15cb1d04e940aefac53b7aa3b0691adc
Joe Huang <joe@masterbee.co>
2016-11-24 23:00:35 -0500

    addend systerm installation command

---
+++ System Installation/README.md
@@ -0,0 +1,7 @@
+With scripts in this folder, we will be able to setup system environment with one command.
+
+### Install basic admin tools.
+
+```bash
+curl -sSL https://goo.gl/ | bash
+```
---
+++ System Installation/system-tool/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get upgrade -y
+
+
+# Install apt-fast
+#sudo add-apt-repository ppa:saiarcot895/myppa
+#sudo apt-get update
+#sudo apt-get -y install apt-fast
+
+
+  # Install netselect
+  #  sudo wget http://http.us.debian.org/debian/pool/main/n/netselect/netselect-apt_0.3.ds1-25_all.deb | dpkg -i
+  #  sudo apt-get install netselect-apt
+
+# Install basic tools
+
+sudo apt-get install -y \
+  git \
+  htop \
+  nload \
+  sysstat \
+  iptraf \
