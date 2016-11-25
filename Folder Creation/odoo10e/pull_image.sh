4673b30a18deff2662436bb5010ab265afa972f8
Joe Huang <joe@masterbee.co>
2016-11-25 16:40:27 -0500

    minor update

--- Folder Creation/odoo10e/pull_image.sh
+++ Folder Creation/odoo10e/pull_image.sh
@@ -7,8 +7,7 @@ docker pull razeen/odoo10e:v1.1
 }
 
 read -r -p "Do you wish to pull down all images? [y/N]" response
-case $response in
-    [yY][eE][sS]|[yY])
+case $response in [yY][eE][sS]|[yY])
       image_pull
       echo "Image Pull Secussful."
         ;;
