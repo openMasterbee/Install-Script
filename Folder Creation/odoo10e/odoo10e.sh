6eafa6a5f856b45f347e7f26b186f3aed7c71806
Joe Huang <joe@masterbee.co>
2016-11-25 16:09:22 -0500

    update image_pull function

--- Folder Creation/odoo10e/odoo10e.sh
+++ Folder Creation/odoo10e/odoo10e.sh
@@ -92,6 +92,14 @@ echo "----------------------------------------------------------------"
 echo ""
 
 
+image_pull () {
+docker pull postgres:9.3
+docker pull redis
+docker pull nginx:alpine
+docker pull tozny/rancher-lets-encrypt
+docker pull razeen/odoo10e:v1.1
+}
+
 read -r -p "Do you wish to pull down all images? [y/N]" response
 case $response in
     [yY][eE][sS]|[yY])
@@ -102,12 +110,3 @@ case $response in
       echo "Image pulling skiped. Process finish."
         ;;
 esac
-
-
-image_pull () {
-docker pull postgres:9.3
-docker pull redis
-docker pull nginx:alpine
-docker pull tozny/rancher-lets-encrypt
-docker pull razeen/odoo10e:v1.1
-}
